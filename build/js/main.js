68b5995de77d5dde953ed58d3351a5d39bde77b4
Vasya <boyko1396@gmail.com>
2023-08-08 18:39:42 +0300

    fix 08.08.2023

--- build/js/main.js
+++ build/js/main.js
@@ -1,89 +1,93 @@
-if (document.querySelector('.js-profile-nav')) {
-  // header dropdown toggle mobile
-  const headerBtnToggle = document.querySelectorAll('.js-header-toggle');
+document.addEventListener('DOMContentLoaded', function() {
+  if (document.querySelector('.js-profile-nav')) {
+    // header dropdown toggle mobile
+    const headerBtnToggle = document.querySelectorAll('.js-header-toggle');
 
-  headerBtnToggle.forEach(function(button) {
-    button.addEventListener('click', function() {
-      document.body.classList.toggle('is-hidden-mobile');
-      document.body.classList.toggle('is-open-menu-mobile');
+    headerBtnToggle.forEach(function(button) {
+      button.addEventListener('click', function() {
+        document.body.classList.toggle('is-hidden-mobile');
+        document.body.classList.toggle('is-open-menu-mobile');
+      });
     });
-  });
 
-  // header profile nav dropdown
-  const headerProfileBtn = document.querySelector('.js-profile-nav');
-  const headerProfileNav = document.querySelector('.header__top-aside-profile');
+    // header profile nav dropdown
+    const headerProfileBtn = document.querySelector('.js-profile-nav');
+    const headerProfileNav = document.querySelector('.header__top-aside-profile');
 
-  headerProfileBtn.addEventListener('click', function(event) {
-    if (headerProfileNav.classList.contains('is-open')) {
-      headerProfileNav.classList.remove('is-open');
-    } else {
-      headerProfileNav.classList.add('is-open');
-    }
-    event.stopPropagation();
-  });
+    headerProfileBtn.addEventListener('click', function(event) {
+      if (headerProfileNav.classList.contains('is-open')) {
+        headerProfileNav.classList.remove('is-open');
+      } else {
+        headerProfileNav.classList.add('is-open');
+      }
+      event.stopPropagation();
+    });
 
-  document.addEventListener('click', function() {
-    if (event.target !== headerProfileBtn && !headerProfileBtn.contains(event.target)) {
-      headerProfileNav.classList.remove('is-open');
-    }
-  });
-}
+    document.addEventListener('click', function() {
+      if (event.target !== headerProfileBtn && !headerProfileBtn.contains(event.target)) {
+        headerProfileNav.classList.remove('is-open');
+      }
+    });
+  }
 
-// banner btn close
-const closeButtonBlockBF = document.querySelector('.js-block-b-f-close');
+  if (document.querySelector('.js-block-b-f-close')) {
+    const closeButtonBlockBF = document.querySelector('.js-block-b-f-close');
+    // banner btn close
 
-closeButtonBlockBF.addEventListener('click', () => {
-  const blockBF = closeButtonBlockBF.closest('.block-b-f');
-  blockBF.classList.add('is-hidden');
-});
+    closeButtonBlockBF.addEventListener('click', () => {
+      const blockBF = closeButtonBlockBF.closest('.block-b-f');
+      blockBF.classList.add('is-hidden');
+    });
+  }
 
-// btn bookmark toggle
-const bookmarkBtn = document.querySelectorAll('.js-bookmark-btn');
+  // btn bookmark toggle
+  if (document.querySelector('.js-bookmark-btn')) {
+    const bookmarkBtn = document.querySelectorAll('.js-bookmark-btn');
 
-bookmarkBtn.forEach(function(btn) {
-  btn.addEventListener('click', function() {
-    this.classList.toggle('is-active');
-  });
-});
+    bookmarkBtn.forEach(function(btn) {
+      btn.addEventListener('click', function() {
+        this.classList.toggle('is-active');
+      });
+    });
+  }
 
-// modal action init
-const modalActions = document.querySelectorAll('.modal-action');
-const modalActionDropdown = document.querySelector('.modal-action__dropdown');
+  // modal action init
+  if (document.querySelector('.modal-action')) {
+    const modalActions = document.querySelectorAll('.modal-action');
+    const modalActionDropdown = document.querySelector('.modal-action__dropdown');
 
-modalActions.forEach(function(modalAction) {
-  const modalActionInit = modalAction.querySelector('.js-modal-action-init');
+    modalActions.forEach(function(modalAction) {
+      const modalActionInit = modalAction.querySelector('.js-modal-action-init');
 
-  modalActionInit.addEventListener('click', function(event) {
-    // Toggle the 'is-active' class on the current '.modal-action' element
-    modalAction.classList.toggle('is-active');
+      modalActionInit.addEventListener('click', function(event) {
+        modalAction.classList.toggle('is-active');
 
-    // Toggle the 'is-active' class on the '.js-modal-action-init' button
-    modalActionInit.classList.toggle('is-active');
+        modalActionInit.classList.toggle('is-active');
 
-    event.stopPropagation(); // Stop further propagation of the click event
-  });
+        event.stopPropagation();
+      });
 
-  document.addEventListener('click', function(event) {
-    // Check if the click occurred on elements not belonging to the current modal
-    if (
-      event.target !== modalAction &&
-      event.target !== modalActionDropdown &&
-      !modalAction.contains(event.target)
-    ) {
-      // Remove the 'is-active' class from the current '.modal-action' element
-      modalAction.classList.remove('is-active');
+      document.addEventListener('click', function(event) {
+        if (
+          event.target !== modalAction &&
+          event.target !== modalActionDropdown &&
+          !modalAction.contains(event.target)
+        ) {
+          modalAction.classList.remove('is-active');
+          modalActionInit.classList.remove('is-active');
+        }
+      });
+    });
+  }
 
-      // Remove the 'is-active' class from the '.js-modal-action-init' button
-      modalActionInit.classList.remove('is-active');
-    }
-  });
-});
+  // btn-moore toggle
+  if (document.querySelector('.js-btn-moore-toggle')) {
+    const btnMooreTitle = document.querySelectorAll('.js-btn-moore-toggle');
 
-// btn-moore toggle
-const btnMooreTitle = document.querySelectorAll('.js-btn-moore-toggle');
-
-btnMooreTitle.forEach(function(btn) {
-  btn.addEventListener('click', function() {
-    this.classList.toggle('is-active');
-  });
+    btnMooreTitle.forEach(function(btn) {
+      btn.addEventListener('click', function() {
+        this.classList.toggle('is-active');
+      });
+    });
+  }
 });
